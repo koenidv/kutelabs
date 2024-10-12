17c806dd5de3f0ec4fa20a01d3cc1fd05c11f52a
Florian König <32238636+koenidv@users.noreply.github.com>
2024-10-12 18:48:40 +0200

    server:feat cache transpiled code

--- server/src/routes/transpile/index.ts
+++ server/src/routes/transpile/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono"
 import { ResultDTO, TranspilatonError } from "./ResultDTO"
 import { transpile } from "../../transpile/transpile"
+import { existsInCache, readTranspiledCache, writeTranspiledCache } from "../../cache/cacheUtils"
 
 const app = new Hono()
 
@@ -10,7 +11,12 @@ app.post("/kt/js", async c => {
 
   if (!kotlinCode) return c.json(ResultDTO.error(TranspilatonError.noCode))
 
+  if (await existsInCache(kotlinCode))
+    return c.json(ResultDTO.cached(await readTranspiledCache(kotlinCode)))
+
   const transpiled = await transpile(kotlinCode)
+  writeTranspiledCache(kotlinCode, transpiled.transpiled!)
+
   return c.json(ResultDTO.fromTranspilationResult(transpiled))
 })
 
