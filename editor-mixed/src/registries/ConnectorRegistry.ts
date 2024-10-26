1a25e2b260b044209bebcf21fca5061745edda94
Florian König <32238636+koenidv@users.noreply.github.com>
2024-10-26 21:55:45 +0200

    mce:refactor prettify ConnectorRegistry

--- editor-mixed/src/registries/ConnectorRegistry.ts
+++ editor-mixed/src/registries/ConnectorRegistry.ts
@@ -22,10 +22,14 @@ export class ConnectorRegistry {
     this._connectors.push(connector)
   }
 
-  public selectConnectorForBlock(block: Block, dragOffset: Coordinates, maxXY: number = 25): Connection | null {
+  public selectConnectorForBlock(
+    block: Block,
+    dragOffset: Coordinates,
+    maxXY: number = 25
+  ): Connection | null {
     let localConnector: Connector | null = null
     let remoteConnector: Connector | null = null
-    const connectedIds =[
+    const connectedIds = [
       block.id,
       ...block.allConnectedRecursive.map(({ id }) => id),
     ]
@@ -33,46 +37,84 @@ export class ConnectorRegistry {
     // Regular before->after / before->extension / before->inner
     localConnector = block.connectors.before
     if (localConnector) {
-      remoteConnector = this.getClosestConnector(connectedIds, Coordinates.add(localConnector.globalPosition, dragOffset), [ConnectorType.After, ConnectorType.Extension, ConnectorType.Inner], maxXY)
-      if (remoteConnector) return new Connection(localConnector, remoteConnector)
+      remoteConnector = this.getClosestConnector(
+        connectedIds,
+        Coordinates.add(localConnector.globalPosition, dragOffset),
+        [ConnectorType.After, ConnectorType.Extension, ConnectorType.Inner],
+        maxXY
+      )
+      if (remoteConnector)
+        return new Connection(localConnector, remoteConnector)
     }
 
     // reverse after->before
     localConnector = block.lastAfter.connectors.after
     if (localConnector) {
       // const offset = subtractCoordinates(localConnector.calculatedPosition, block.calculatedPosition)
-      remoteConnector = this.getClosestConnector(connectedIds, Coordinates.add(localConnector.globalPosition, dragOffset), [ConnectorType.Before], maxXY)
-      if (remoteConnector) return new Connection(localConnector, remoteConnector)
+      remoteConnector = this.getClosestConnector(
+        connectedIds,
+        Coordinates.add(localConnector.globalPosition, dragOffset),
+        [ConnectorType.Before],
+        maxXY
+      )
+      if (remoteConnector)
+        return new Connection(localConnector, remoteConnector)
     }
 
     return null
   }
 
-  public getClosestConnector(ignoreIds: string[], position: Coordinates, types: ConnectorType[], maxXY: number): Connector | null {
-    const connectors = this.getNearbyConnectors(ignoreIds, position, types, maxXY)
+  public getClosestConnector(
+    ignoreIds: string[],
+    position: Coordinates,
+    types: ConnectorType[],
+    maxXY: number
+  ): Connector | null {
+    const connectors = this.getNearbyConnectors(
+      ignoreIds,
+      position,
+      types,
+      maxXY
+    )
     if (connectors.length === 0) return null
     return this.sortConnectorsByDistance(position, connectors)[0]
   }
 
-  public getNearbyConnectors(ingoreIds: string[], position: Coordinates, types: ConnectorType[], maxXY: number): Connector[] {
+  public getNearbyConnectors(
+    ingoreIds: string[],
+    position: Coordinates,
+    types: ConnectorType[],
+    maxXY: number
+  ): Connector[] {
     return this._connectors.filter(connector => {
       if (connector.parentBlock === null) return false
       if (ingoreIds.includes(connector.parentBlock.id)) {
         return false
       }
       if (types.includes(connector.type)) {
-        return Math.abs(connector.globalPosition.x - position.x) <= maxXY && Math.abs(connector.globalPosition.y - position.y) <= maxXY
+        return (
+          Math.abs(connector.globalPosition.x - position.x) <= maxXY &&
+          Math.abs(connector.globalPosition.y - position.y) <= maxXY
+        )
       }
       return false
     })
   }
 
-  public sortConnectorsByDistance(position: Coordinates, connectors: Connector[]): Connector[] {
+  public sortConnectorsByDistance(
+    position: Coordinates,
+    connectors: Connector[]
+  ): Connector[] {
     return connectors.sort((a, b) => {
-      const aDistance = Math.sqrt(Math.pow(a.globalPosition.x - position.x, 2) + Math.pow(a.globalPosition.y - position.y, 2))
-      const bDistance = Math.sqrt(Math.pow(b.globalPosition.x - position.x, 2) + Math.pow(b.globalPosition.y - position.y, 2))
+      const aDistance = Math.sqrt(
+        Math.pow(a.globalPosition.x - position.x, 2) +
+          Math.pow(a.globalPosition.y - position.y, 2)
+      )
+      const bDistance = Math.sqrt(
+        Math.pow(b.globalPosition.x - position.x, 2) +
+          Math.pow(b.globalPosition.y - position.y, 2)
+      )
       return aDistance - bDistance
     })
   }
-
 }
