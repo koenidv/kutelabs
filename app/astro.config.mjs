eaf13f9fd0dbd4d180208097288ca2f8b31890cd
Florian König <32238636+koenidv@users.noreply.github.com>
2024-10-27 18:33:33 +0100

    devops: update readme, fix astro config

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import lit from '@astrojs/lit';
-
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import lit from "@astrojs/lit"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), lit()],
-  output: 'static',
-  // adapter: netlify()
-});
+  output: "static"
+})
