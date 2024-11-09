b451c70409c74888dcf98ccdc9abb96422fa7a2a
Florian König <32238636+koenidv@users.noreply.github.com>
2024-11-09 12:19:59 +0100

    shared:fix make extension non-enumerable

--- shared/src/extensions.ts
+++ shared/src/extensions.ts
@@ -1,3 +1,4 @@
+// create non-enumerable properties to not break prism
 declare global {
   interface Object {
     also<T>(this: T, block: (it: T) => void): T
@@ -11,20 +12,35 @@ declare global {
   }
 }
 
-Object.prototype.also = function <T>(this: T, block: (it: T) => void): T {
-  block(this)
-  return this
-}
+Object.defineProperty(Object.prototype, "also", {
+  value: function <T>(this: T, block: (it: T) => void): T {
+    block(this)
+    return this
+  },
+  enumerable: false,
+  writable: true,
+  configurable: true,
+})
 
-Object.prototype.let = function <T, R>(
-  this: T | null | undefined,
-  block: (it: T) => R
-): R | null | undefined {
-  if (this === null) return null
-  if (this === undefined) return undefined
-  return block(this)
-}
+Object.defineProperty(Object.prototype, "let", {
+  value: function <T, R>(
+    this: T | null | undefined,
+    block: (it: T) => R
+  ): R | null | undefined {
+    if (this === null) return null
+    if (this === undefined) return undefined
+    return block(this)
+  },
+  enumerable: false,
+  writable: true,
+  configurable: true,
+})
 
-Array.prototype.firstOrNull = function <T>(this: T[]): T | null {
-  return this.length > 0 ? this[0] : null
-}
+Object.defineProperty(Array.prototype, "firstOrNull", {
+  value: function <T>(this: T[]): T | null {
+    return this.length > 0 ? this[0] : null
+  },
+  enumerable: false,
+  writable: true,
+  configurable: true,
+})
