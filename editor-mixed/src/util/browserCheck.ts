4295f2e490d2a76e75fbb5f62dfdeb014e33b1be
Florian König <32238636+koenidv@users.noreply.github.com>
2024-12-01 12:10:09 +0100

    mce:fix navigator not defined during ssr

--- editor-mixed/src/util/browserCheck.ts
+++ editor-mixed/src/util/browserCheck.ts
@@ -1,1 +1,4 @@
-export var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export var isSafari =
+  typeof window !== "undefined" && typeof navigator !== "undefined"
+    ? /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+    : false
