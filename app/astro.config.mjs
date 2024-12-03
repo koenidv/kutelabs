fdca6bbad4c707798f81a8166c3fb7b09405734e
Florian König <32238636+koenidv@users.noreply.github.com>
2024-12-03 23:35:56 +0100

    app:chore add clark

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config"
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind"
 import lit from "@astrojs/lit"
+import clerk from "@clerk/astro";
 
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit(), sentry(), spotlightjs()],
-  output: "static"
+  integrations: [tailwind(), lit(), clerk(), sentry(), spotlightjs()],
+  output: "server",
+  adapter: netlify()
 })
