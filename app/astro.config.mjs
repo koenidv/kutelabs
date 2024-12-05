61b791ec124c1eee173c15962227e9f05faedaa1
Florian König <32238636+koenidv@users.noreply.github.com>
2024-12-05 14:49:41 +0100

    app:chore remove clerk

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config"
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind"
 import lit from "@astrojs/lit"
-import clerk from "@clerk/astro";
 
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit(), clerk(), sentry(), spotlightjs()],
+  integrations: [tailwind(), lit(), sentry(), spotlightjs()],
   output: "server",
   adapter: netlify()
 })
