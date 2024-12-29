b467bceb3e1668f2c30c58d92543b35e6155aac9
Florian König <32238636+koenidv@users.noreply.github.com>
2024-12-29 17:29:50 +0100

    app:chore remove deprecated lit, add svelte

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,17 +2,16 @@
 import { defineConfig, envField } from "astro/config"
 import netlify from "@astrojs/netlify"
 import tailwind from "@astrojs/tailwind"
-import lit from "@astrojs/lit"
 import compress from "astro-compress";
 import icon from "astro-icon";
-
+import svelte from "@astrojs/svelte";
 
 // import sentry from "@sentry/astro";
 // import spotlightjs from "@spotlightjs/astro"; removed for now because they break the dev server with SSG
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), lit(), compress()],
+  integrations: [tailwind(), icon(), svelte(), compress()],
   output: "static",
   adapter: netlify(),
   env: {
