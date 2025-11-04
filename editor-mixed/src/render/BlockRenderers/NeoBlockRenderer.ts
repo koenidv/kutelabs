4490725937bd48e00f309489a5698e0eb612b7c5
Florian König <32238636+koenidv@users.noreply.github.com>
2025-11-04 18:22:14 +0100

    chore: neo render function return

--- editor-mixed/src/render/BlockRenderers/NeoBlockRenderer.ts
+++ editor-mixed/src/render/BlockRenderers/NeoBlockRenderer.ts
@@ -132,11 +132,16 @@ export class NeoBlockRenderer extends BaseBlockRenderer {
 
     connectors.forEach(connector => {
       if (connector.type === ConnectorType.Internal) return
-      if (
-        connector.type === ConnectorType.Before &&
-        block.upstream?.connectedBlocks.byBlock(block)?.type === ConnectorType.Inner
-      )
-        return
+      const upstreamConnected = block.upstream?.connectedBlocks.byBlock(block)
+      if (connector.type === ConnectorType.Before && upstreamConnected) {
+        if (
+          upstreamConnected.type === ConnectorType.Inner &&
+          upstreamConnected.role === ConnectorRole.Input
+        )
+          return
+        if (upstreamConnected.role === ConnectorRole.Conditional) return
+        if (upstreamConnected.role === ConnectorRole.Output) return
+      }
 
       rectangle.add(
         {
@@ -271,7 +276,7 @@ export class NeoBlockRenderer extends BaseBlockRenderer {
     const { block, size } = registered
 
     const head = svg`
-    <text x=${-size.heads[0] / 2} y="5" transform="rotate(270)" text-anchor="middle" alignment-baseline="hanging" opacity="0.65" fill="white">fun</text>
+      <text x=${-size.heads[0] / 2} y="5" transform="rotate(270)" text-anchor="middle" alignment-baseline="hanging" opacity="0.65" fill="white">fun</text>
       ${
         block.data.isMain || block.data.nameEditable === false
           ? svg`<text x="24" y=${size.heads[0] / 2} fill="white" alignment-baseline="middle">${block.data.name}</text>`
@@ -286,6 +291,10 @@ export class NeoBlockRenderer extends BaseBlockRenderer {
               props
             )
       }
+      <text x=${size.fullWidth - size.rightWidth - 112} y=${size.fullHeight - size.fullTailHeight / 2} text-anchor="end" alignment-baseline="middle" opacity="1" fill="white">returns</text>
+      <g transform="translate(${size.fullWidth - size.rightWidth - 58}, ${size.fullHeight - size.fullTailHeight / 2 - 16}) rotate(90) scale(0.1, 0.125)" fill="white">
+        <path d="M170.71,303.132c-1.836-2.448-4.896-2.448-6.732,0c-3.06,3.672-1.836,7.956-1.836,12.853 c0,6.12-0.612,12.239-1.836,18.359c-1.224,4.896,6.12,6.732,7.956,2.448c1.837-4.896,3.061-10.404,3.673-15.912 C172.547,314.76,174.383,308.028,170.71,303.132z"></path> <path d="M171.322,277.428c-1.224-3.672-1.836-6.731-2.449-10.403c0-1.836,0.612-3.672,0-5.509 c-0.612-1.836-0.612-3.06-1.835-4.896c-1.224-2.448-5.508-2.448-6.732,0c-1.836,4.284-1.836,7.344-0.612,11.628 s1.836,7.956,4.284,12.24C166.426,284.16,172.547,281.712,171.322,277.428z"></path> <path d="M167.038,202.152c-1.224-1.836-4.284-1.836-6.12,0c-2.448,3.06-1.836,5.508-1.836,9.18s0.612,6.732,0.612,10.404 c0.612,4.896,7.344,4.896,7.344,0c0-3.061,0.612-6.12,0.612-9.181C168.874,208.884,170.099,205.824,167.038,202.152z"></path> <path d="M167.65,174.612c-2.448-6.732-1.224-13.464-3.06-20.196c-1.224-3.672-6.732-3.672-7.956,0 c-2.448,7.344,0,15.912,4.284,22.645C162.754,180.732,168.874,178.284,167.65,174.612z"></path> <path d="M163.366,96.276c-1.224-1.224-3.672-1.224-4.896,0c-3.672,3.672-2.448,8.568-2.448,13.464c0,4.284,0,9.18,0.612,13.464 c0.612,3.672,6.732,3.672,7.344,0c0.612-4.284,0.612-7.956,0.612-12.24C165.814,106.068,167.038,100.56,163.366,96.276z"></path> <path d="M157.246,43.644c-3.672,0-6.12,3.672-4.896,6.732c1.224,4.896,1.224,11.016,1.836,15.912c0.612,3.06,6.12,3.06,6.732,0 c0.612-5.508,0-11.016,1.836-15.912C163.366,47.316,160.918,43.644,157.246,43.644z"></path> <path d="M215.387,41.196c-14.076-15.3-29.376-30.6-48.349-40.392c-2.448-1.224-5.508-1.224-7.344,1.224 c-12.852,15.3-22.644,32.436-36.108,47.124c-4.896,4.896,3.06,12.24,7.344,7.344c12.24-13.464,21.42-29.376,33.66-42.84 c15.912,9.792,26.929,23.868,41.005,36.72C212.326,56.496,220.895,47.316,215.387,41.196z"></path>
+      </g>
     `
 
     let params: TemplateResult<2>[] = []
